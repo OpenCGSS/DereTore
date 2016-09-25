c54d318033ddaffa9fd79fded2a395282c384f80
George Wu <uiharu@buaa.edu.cn>
2016-09-25 03:36:38 +0200

    Fixed rare crash when finding the clicked ScoreBar.

--- DereTore.Applications.StarlightDirector/UI/Controls/ScoreEditor.xaml.cs
+++ DereTore.Applications.StarlightDirector/UI/Controls/ScoreEditor.xaml.cs
@@ -100,8 +100,17 @@ namespace DereTore.Applications.StarlightDirector.UI.Controls {
                 scoreNote.IsSelected = true;
             }
             var hitPosition = e.GetPosition(BarLayer);
-            var scoreBarIndex = (int)((ScrollOffset + hitPosition.Y) / BarHeight);
-            SelectScoreBar(ScoreBars[scoreBarIndex]);
+            var hitResult = VisualTreeHelper.HitTest(BarLayer, hitPosition);
+            var visual = hitResult.VisualHit as FrameworkElement;
+            ScoreBar hitScoreBar = null;
+            while (visual != null) {
+                if (visual is ScoreBar) {
+                    hitScoreBar = visual as ScoreBar;
+                    break;
+                }
+                visual = visual.Parent as FrameworkElement;
+            }
+            SelectScoreBar(hitScoreBar);
             if (scoreNote.IsSelected && EditMode != EditMode.Select && EditMode != EditMode.Clear) {
                 switch (EditMode) {
                     case EditMode.Sync:
