OpenCGSS/DereTore

commit 258f194a46142ebd9c7cb613787495d186d9b389

[director] Fixes the hold note connection issue when importing Deleste beatmaps.

DereTore.Applications.StarlightDirector/Entities/Note.cs
@@ -12,7 +12,7 @@ namespace DereTore.Applications.StarlightDirector.Entities {
         public int ID { get; private set; }
 
         [JsonProperty]
-        public int PositionInGrid { get; set; }
+        public int IndexInGrid { get; set; }
 
         public NoteType Type {
             get { return (NoteType)GetValue(TypeProperty); }
@@ -31,7 +31,7 @@ namespace DereTore.Applications.StarlightDirector.Entities {
             set { SetValue(FinishPositionProperty, value); }
         }
 
-        public int PositionInTrack => (int)FinishPosition - 1;
+        public int IndexInTrack => (int)FinishPosition - 1;
 
         [JsonProperty]
         public NoteFlickType FlickType {
@@ -195,7 +195,7 @@ namespace DereTore.Applications.StarlightDirector.Entities {
 
         public static readonly Comparison<Note> TimingComparison = (n1, n2) => {
             if (n1.Bar == n2.Bar) {
-                return n1.PositionInGrid.CompareTo(n2.PositionInGrid);
+                return n1.IndexInGrid.CompareTo(n2.IndexInGrid);
             } else {
                 return n1.GetHitTiming().CompareTo(n2.GetHitTiming());
             }
@@ -282,7 +282,7 @@ namespace DereTore.Applications.StarlightDirector.Entities {
         internal Note(int id, Bar bar) {
             ID = id;
             Bar = bar;
-            PositionInGrid = 0;
+            IndexInGrid = 0;
             Type = NoteType.TapOrFlick;
             StartPosition = NotePosition.Nowhere;
             FinishPosition = NotePosition.Nowhere;

DereTore.Applications.StarlightDirector/Conversion/Formats/Deleste/DelesteBasicNote.cs
@@ -29,7 +29,7 @@ namespace DereTore.Applications.StarlightDirector.Conversion.Formats.Deleste {
 
         public bool IsHoldStart => Type == DelesteNoteType.Hold;
 
-        public int PositionInTrack => (int)FinishPosition - 1;
+        public int IndexInTrack => (int)FinishPosition - 1;
 
         public DelesteBeatmapEntry Entry { get; internal set; }
 

DereTore.Applications.StarlightDirector/Conversion/Formats/Deleste/DelesteHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -26,7 +27,7 @@ namespace DereTore.Applications.StarlightDirector.Conversion.Formats.Deleste {
                 var timePerBeat = DirectorUtilities.BpmToSeconds(bpm);
                 var totalBarGridCount = bar.GetTotalGridCount();
 
-                if (!totalBarGridCount.IsMultipleOf(entry.FullLength)) {
+                if (entry.FullLength > 0 && !totalBarGridCount.IsMultipleOf(entry.FullLength)) {
                     // Try to fit in the grid. E.g. Yumeiro Harmony measure #041.
                     var lengthFactors = new List<uint> {
                         (uint)entry.FullLength
@@ -53,13 +54,18 @@ namespace DereTore.Applications.StarlightDirector.Conversion.Formats.Deleste {
                         lastFlickNote = null;
                     }
                     var note = bar.AddNote();
-                    note.PositionInGrid = (int)(totalBarGridCount * ((float)basicNote.IndexInMeasure / entry.FullLength));
+                    note.IndexInGrid = (int)(totalBarGridCount * ((float)basicNote.IndexInMeasure / entry.FullLength));
                     note.StartPosition = basicNote.StartPosition;
                     note.FinishPosition = basicNote.FinishPosition;
-                    var positionInTrack = basicNote.PositionInTrack;
+                    var positionInTrack = basicNote.IndexInTrack;
 
                     if (basicNote.IsHoldStart) {
-                        if (currentHoldingNotes[positionInTrack] != null) {
+                        // The '<' part fixes this example: (credits to @inosuke01)
+                        // #0,043:330000002020*4*000:42511:12132
+                        // #1,043:4000202040002000:11211:44544
+                        // #4,043:0001*1*00000000000:32
+                        // Without this judgement, the two notes marked with '*' will be wrongly connected.
+                        if (currentHoldingNotes[positionInTrack] != null && currentHoldingNotes[positionInTrack] < note) {
                             // Invalid holding notes: I haven't released my finger yet!
                             // OK, accept it, set the newly added note as hold end.
                             Note.ConnectHold(currentHoldingNotes[positionInTrack], note);
@@ -71,7 +77,7 @@ namespace DereTore.Applications.StarlightDirector.Conversion.Formats.Deleste {
                         lastBasicFlickNote = null;
                         continue;
                     }
-                    if (currentHoldingNotes[positionInTrack] != null) {
+                    if (currentHoldingNotes[positionInTrack] != null && currentHoldingNotes[positionInTrack] < note) {
                         Note.ConnectHold(currentHoldingNotes[positionInTrack], note);
                         currentHoldingNotes[positionInTrack] = null;
                         if (basicNote.IsFlick) {
@@ -132,14 +138,14 @@ namespace DereTore.Applications.StarlightDirector.Conversion.Formats.Deleste {
             // Fix sync notes.
             foreach (var bar in score.Bars) {
                 var notes = bar.Notes;
-                var distinctGridLineIndices = notes.Select(note => note.PositionInGrid).Distinct().ToArray();
+                var distinctGridLineIndices = notes.Select(note => note.IndexInGrid).Distinct().ToArray();
                 if (distinctGridLineIndices.Length == notes.Count) {
                     continue;
                 }
                 // There are sync notes in this bar.
                 notes.Sort(Note.TimingComparison);
                 for (var i = 0; i < bar.Notes.Count - 1; ++i) {
-                    if (notes[i].PositionInGrid == notes[i + 1].PositionInGrid) {
+                    if (notes[i].IndexInGrid == notes[i + 1].IndexInGrid) {
                         Note.ConnectSync(notes[i], notes[i + 1]);
                         ++i;
                     }
@@ -147,9 +153,14 @@ namespace DereTore.Applications.StarlightDirector.Conversion.Formats.Deleste {
             }
 
             // Fix hold notes: if any line crosses other note(s). (Is it necessary? Deleste files seem to be organized well.)
-            //foreach (var bar in score.Bars) {
-            //    var notes = bar.Notes;
-            //}
+            foreach (var note in score.Notes) {
+                if (note.IsHoldStart) {
+                    var between = score.Notes.GetFirstNoteBetween(note, note.HoldTarget);
+                    if (between != null) {
+                        note.HoldTarget = between;
+                    }
+                }
+            }
         }
 
         public static DelesteBeatmapEntry ReadEntry(Project temporaryProject, string line, int entryCounter, List<DelesteBasicNote> noteCache, List<string> warnings, ref bool hasErrors) {

DereTore.Applications.StarlightDirector/Conversion/ProjectIO.Legacy.cs
@@ -92,7 +92,7 @@ namespace DereTore.Applications.StarlightDirector.Conversion {
                         var score = project.GetScore(difficulty);
                         foreach (var bar in score.Bars) {
                             foreach (var note in bar.Notes) {
-                                note.PositionInGrid *= k;
+                                note.IndexInGrid *= k;
                             }
                         }
                     }

DereTore.Applications.StarlightDirector/Entities/Bar.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace DereTore.Applications.StarlightDirector.Entities {
-    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy), MemberSerialization = MemberSerialization.OptIn)]
     public sealed class Bar {
 
         public Note AddNote() {
@@ -13,16 +13,28 @@ namespace DereTore.Applications.StarlightDirector.Entities {
             }
             var note = new Note(id, this);
             Notes.Add(note);
+            Score.Notes.Add(note);
             return note;
         }
 
+        public bool RemoveNote(Note note) {
+            if (Notes.Contains(note)) {
+                return false;
+            }
+            Notes.Remove(note);
+            Score.Notes.Remove(note);
+            return true;
+        }
+
+        [JsonProperty]
         public InternalList<Note> Notes { get; }
 
+        [JsonProperty]
         public BarParams Params { get; internal set; }
 
+        [JsonProperty]
         public int Index { get; internal set; }
 
-        [JsonIgnore]
         public Score Score { get; internal set; }
 
         [JsonConstructor]
@@ -32,7 +44,7 @@ namespace DereTore.Applications.StarlightDirector.Entities {
             Index = index;
         }
 
-        public void SquashParams() {
+        internal void SquashParams() {
             if (Params?.CanBeSquashed ?? false) {
                 Params = null;
             }

DereTore.Applications.StarlightDirector/Entities/Score.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Input;
+﻿using System.Linq;
 using DereTore.Applications.StarlightDirector.Components;
 using DereTore.Applications.StarlightDirector.Extensions;
 using Newtonsoft.Json;
@@ -67,20 +64,35 @@ namespace DereTore.Applications.StarlightDirector.Entities {
             if (index < 0 || index >= Bars.Count) {
                 return false;
             }
-            Bars.RemoveAt(index);
-            for (var i = index; i < Bars.Count; ++i) {
-                --Bars[i].Index;
+            return RemoveBar(Bars[index]);
+        }
+
+        public bool RemoveBar(Bar bar) {
+            var bars = Bars;
+            if (!bars.Contains(bar)) {
+                return false;
+            }
+            var index = bars.IndexOf(bar);
+            foreach (var note in bar.Notes) {
+                Notes.Remove(note);
+            }
+            bars.Remove(bar);
+            for (var i = index; i < bars.Count; ++i) {
+                --bars[i].Index;
             }
             return true;
         }
 
-        public bool HasAnyNote => Bars.Count > 0 && Bars.Any(bar => bar.Notes.Count > 0);
+        public bool HasAnyNote => Notes.Count > 0;
+
+        public InternalList<Note> Notes { get; }
 
         [JsonConstructor]
         internal Score(Project project, Difficulty difficulty) {
             Bars = new InternalList<Bar>();
             Project = project;
             Difficulty = difficulty;
+            Notes = new InternalList<Note>();
             _flickGroupIDGenerator = new IntegerIDGenerator(1);
         }
 
@@ -90,14 +102,13 @@ namespace DereTore.Applications.StarlightDirector.Entities {
             }
             Project = project;
             foreach (var bar in Bars) {
-                if (bar.Notes != null) {
-                    foreach (var note in bar.Notes) {
-                        note.Bar = bar;
-                    }
+                foreach (var note in bar.Notes) {
+                    note.Bar = bar;
                 }
                 bar.Score = this;
             }
             var allNotes = Bars.SelectMany(bar => bar.Notes).ToArray();
+            Notes.AddRange(allNotes);
             foreach (var note in allNotes) {
                 if (note.SyncTargetID != EntityID.Invalid) {
                     note.SyncTarget = FindNoteByID(note.SyncTargetID);
@@ -151,7 +162,7 @@ namespace DereTore.Applications.StarlightDirector.Entities {
                     compiledNote.FinishPosition = note.FinishPosition;
                     compiledNote.FlickType = (int)note.FlickType;
                     compiledNote.IsSync = note.IsSync;
-                    compiledNote.HitTiming = endTimeOfLastBar + barTimeInSeconds * (note.PositionInGrid / (double)(signature * gridCountInBar));
+                    compiledNote.HitTiming = endTimeOfLastBar + barTimeInSeconds * (note.IndexInGrid / (double)(signature * gridCountInBar));
                     if (note.GroupID != EntityID.Invalid) {
                         compiledNote.FlickGroupID = note.GroupID;
                     } else {

DereTore.Applications.StarlightDirector/Extensions/NoteExtensions.cs
@@ -1,4 +1,6 @@
-﻿using DereTore.Applications.StarlightDirector.Entities;
+﻿using System.Collections.Generic;
+using System.Linq;
+using DereTore.Applications.StarlightDirector.Entities;
 
 namespace DereTore.Applications.StarlightDirector.Extensions {
     public static class NoteExtensions {
@@ -9,7 +11,42 @@ namespace DereTore.Applications.StarlightDirector.Extensions {
             var signature = bar.GetActualSignature();
             var gridCountInBar = bar.GetActualGridPerSignature();
             var barLength = bar.GetLength();
-            return barStartTime + barLength * (note.PositionInGrid / (double)(signature * gridCountInBar));
+            return barStartTime + barLength * (note.IndexInGrid / (double)(signature * gridCountInBar));
+        }
+
+        public static Note GetFirstNoteBetween(this IEnumerable<Note> notes, Note n1, Note n2) {
+            var first = n1 < n2 ? n1 : n2;
+            var second = first.Equals(n1) ? n2 : n1;
+            foreach (var n in notes) {
+                if (n.Equals(first) || n.Equals(second)) {
+                    continue;
+                }
+                if (n.FinishPosition != first.FinishPosition || first.Bar.Index > n.Bar.Index || n.Bar.Index > second.Bar.Index) {
+                    continue;
+                }
+                if (first.Bar.Index == second.Bar.Index) {
+                    if (first.IndexInGrid <= n.IndexInGrid && n.IndexInGrid <= second.IndexInGrid) {
+                        return n;
+                    }
+                } else {
+                    if (first.Bar.Index == n.Bar.Index) {
+                        if (first.IndexInGrid <= n.IndexInGrid) {
+                            return n;
+                        }
+                    } else if (second.Bar.Index == n.Bar.Index) {
+                        if (n.IndexInGrid <= second.IndexInGrid) {
+                            return n;
+                        }
+                    } else {
+                        return n;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static bool AnyNoteBetween(this IEnumerable<Note> notes, Note start, Note end) {
+            return GetFirstNoteBetween(notes, start, end) != null;
         }
 
     }

DereTore.Applications.StarlightDirector/UI/Controls/ScoreEditor.Logic.Internal.cs
@@ -32,7 +32,7 @@ namespace DereTore.Applications.StarlightDirector.UI.Controls {
                 var scoreBar = AddScoreBar(null, false, bar);
                 foreach (var note in bar.Notes) {
                     NoteIDs.ExistingIDs.Add(note.ID);
-                    var scoreNote = AddScoreNote(scoreBar, note.PositionInGrid, note.FinishPosition, note);
+                    var scoreNote = AddScoreNote(scoreBar, note.IndexInGrid, note.FinishPosition, note);
                     temporaryMap.Add(note, scoreNote);
                     allNotes.Add(note);
                 }
@@ -128,8 +128,8 @@ namespace DereTore.Applications.StarlightDirector.UI.Controls {
                 var note = scoreNote.Note;
                 var bar = note.Bar;
                 var baseY = scrollOffset + bar.Index * barHeight;
-                var extraY = barHeight * note.PositionInGrid / bar.GetTotalGridCount();
-                scoreNote.X = noteLayerWidth * TrackCenterXPositions[note.PositionInTrack];
+                var extraY = barHeight * note.IndexInGrid / bar.GetTotalGridCount();
+                scoreNote.X = noteLayerWidth * TrackCenterXPositions[note.IndexInTrack];
                 scoreNote.Y = baseY + extraY;
             }
         }
@@ -221,7 +221,7 @@ namespace DereTore.Applications.StarlightDirector.UI.Controls {
                     // The Reset() call is necessary.
                     note.Reset();
                     NoteIDs.ExistingIDs.Remove(note.ID);
-                    note.Bar.Notes.Remove(note);
+                    note.Bar.RemoveNote(note);
                 }
             }
             NoteLayer.Children.Remove(scoreNote);
@@ -275,7 +275,7 @@ namespace DereTore.Applications.StarlightDirector.UI.Controls {
             } else {
                 note = bar.AddNote();
                 note.StartPosition = note.FinishPosition = (NotePosition)(column + 1);
-                note.PositionInGrid = row;
+                note.IndexInGrid = row;
             }
             scoreNote.Note = note;
             EditableScoreNotes.Add(scoreNote);
@@ -383,7 +383,7 @@ namespace DereTore.Applications.StarlightDirector.UI.Controls {
         private ScoreNote AnyNoteExistOnPosition(int barIndex, int column, int row) {
             foreach (var scoreNote in ScoreNotes) {
                 var note = scoreNote.Note;
-                if (note.Bar.Index == barIndex && (int)note.FinishPosition == column + 1 && note.PositionInGrid == row) {
+                if (note.Bar.Index == barIndex && (int)note.FinishPosition == column + 1 && note.IndexInGrid == row) {
                     return scoreNote;
                 }
             }

commit e91a48327dc96a3496d7649800aaf231fd2be2cc

[director] Fixed the grid index issue when opening earlier saved project files, introduced in commit 258f194a.

DereTore.Applications.StarlightDirector/Entities/Note.cs
@@ -10,7 +10,8 @@ namespace DereTore.Applications.StarlightDirector.Entities {
         [JsonProperty]
         public int ID { get; private set; }
 
-        [JsonProperty]
+        // "PositionInGrid" was the first name of this property used in serialization.
+        [JsonProperty("positionInGrid")]
         public int IndexInGrid { get; set; }
 
         public NoteType Type {