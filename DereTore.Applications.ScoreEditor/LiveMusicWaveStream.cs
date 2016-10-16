5f208cd79aaffb226f55ac4209b5f25055554374
statementreply <statementreply@gmail.com>
2016-10-17 02:11:20 +0800

    [score-editor] Accurate SFX playback timing; Music/SFX volume support

---
+++ DereTore.Applications.ScoreEditor/LiveMusicWaveStream.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using DereTore.ACB;
+using DereTore.HCA;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace DereTore.Applications.ScoreEditor {
+    public sealed class LiveMusicWaveStream : WaveStream {
+
+        public static LiveMusicWaveStream FromAcbStream(Stream stream, string acbFileName) {
+            return new LiveMusicWaveStream(stream, acbFileName, DecodeParams.Default);
+        }
+
+        public static LiveMusicWaveStream FromAcbStream(Stream stream, string acbFileName, DecodeParams decodeParams) {
+            return new LiveMusicWaveStream(stream, acbFileName, decodeParams);
+        }
+
+        public static LiveMusicWaveStream FromWaveStream(Stream stream) {
+            return new LiveMusicWaveStream(stream);
+        }
+
+        public static LiveMusicWaveStream FromHcaStream(Stream stream) {
+            return new LiveMusicWaveStream(stream, DecodeParams.Default);
+        }
+
+        public static LiveMusicWaveStream FromHcaStream(Stream stream, DecodeParams decodeParams) {
+            return new LiveMusicWaveStream(stream, decodeParams);
+        }
+
+        public override TimeSpan CurrentTime {
+            get { return _waveStream.CurrentTime; }
+            set {
+                lock (_syncObject) {
+                    var waveStream = _waveStream;
+                    waveStream.CurrentTime = value;
+                    var position = waveStream.Position;
+                    var blockAlign = waveStream.BlockAlign;
+                    if (position % blockAlign != 0) {
+                        position = (long)(Math.Round(position / (double)blockAlign) * blockAlign);
+                        if (position < 0) {
+                            position = 0;
+                        }
+                        waveStream.Position = position;
+                    }
+                }
+            }
+        }
+
+        public Stream SourceStream => _sourceStream;
+
+        public override TimeSpan TotalTime => _waveStream.TotalTime;
+
+        public override long Length => _waveStream.Length;
+
+        public override long Position {
+            get { return _waveStream.Position; }
+            set {
+                lock (_syncObject) {
+                    var waveStream = _waveStream;
+                    var position = value;
+                    var blockAlign = waveStream.BlockAlign;
+                    if (position % blockAlign != 0) {
+                        position = (long)(Math.Round(position / (double)blockAlign) * blockAlign);
+                        if (position < 0) {
+                            position = 0;
+                        }
+                    }
+                    waveStream.Position = position;
+                }
+            }
+        }
+
+        public override WaveFormat WaveFormat => _waveStream.WaveFormat;
+
+        public override int Read(byte[] buffer, int offset, int count) {
+            lock (_syncObject) {
+                return _waveStream.Read(buffer, offset, count);
+            }
+        }
+
+        public string HcaName => _internalName;
+
+        public static bool Check(string acbFileName, string hcaName) {
+            using (var fileStream = File.Open(acbFileName, FileMode.Open, FileAccess.Read)) {
+                return Check(fileStream, acbFileName, hcaName);
+            }
+        }
+
+        public static bool Check(Stream stream, string acbFileName, string hcaName) {
+            try {
+                using (var acb = AcbFile.FromStream(stream, acbFileName, false)) {
+                    if (!acb.FileExists(hcaName)) {
+                        return false;
+                    }
+                    try {
+                        using (var acbStream = acb.OpenDataStream(hcaName)) {
+                            return HcaDecoder.IsHcaStream(acbStream);
+                        }
+                    } catch (HcaException) {
+                        return false;
+                    }
+                }
+            } catch (Exception) {
+                return false;
+            }
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                _waveStream?.Dispose();
+                _hcaDataStream?.Dispose();
+                _acb?.Dispose();
+                _waveStream = null;
+                _hcaDataStream = null;
+                _acb = null;
+            }
+            base.Dispose(disposing);
+            _disposed = true;
+        }
+
+        private LiveMusicWaveStream(Stream acbStream, string acbFileName, DecodeParams decodeParams) {
+            _acb = AcbFile.FromStream(acbStream, acbFileName, false);
+            var names = _acb.GetFileNames();
+            _internalName = names[0];
+            _hcaDataStream = _acb.OpenDataStream(_internalName);
+            var hcaWaveProvider = new HcaWaveProvider(_hcaDataStream, decodeParams);
+            _waveStream = new RawSourceWaveStream(hcaWaveProvider, hcaWaveProvider.WaveFormat);
+            _sourceStream = acbStream;
+            _syncObject = new object();
+        }
+
+        private LiveMusicWaveStream(Stream hcaStream, DecodeParams decodeParams) {
+            var hcaWaveProvider = new HcaWaveProvider(hcaStream, decodeParams);
+            _waveStream = new RawSourceWaveStream(hcaWaveProvider, hcaWaveProvider.WaveFormat);
+            _sourceStream = hcaStream;
+            _syncObject = new object();
+        }
+
+        private LiveMusicWaveStream(Stream waveStream) {
+            _waveStream = new WaveFileReader(waveStream);
+            _sourceStream = waveStream;
+            _syncObject = new object();
+        }
+
+        private bool _disposed = false;
+
+        private readonly Stream _sourceStream;
+        private AcbFile _acb;
+        private Stream _hcaDataStream;
+        private WaveStream _waveStream;
+        private readonly object _syncObject;
+        private readonly string _internalName;
+
+    }
+}
---
+++ DereTore.Applications.ScoreEditor/ScorePlayer.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using AudioOut = NAudio.Wave.WasapiOut;
+
+namespace DereTore.Applications.ScoreEditor {
+    public sealed class ScorePlayer : DisposableBase {
+        public event EventHandler<StoppedEventArgs> PlaybackStopped {
+            add {
+                if (_soundPlayer != null) {
+                    _soundPlayer.PlaybackStopped += value;
+                }
+            }
+            remove {
+                if (_soundPlayer != null) {
+                    _soundPlayer.PlaybackStopped -= value;
+                }
+            }
+        }
+
+        public event EventHandler<EventArgs> PositionChanged;
+
+        public void Play() {
+            if (IsPlaying) {
+                if (IsPaused) {
+                    IsPaused = false;
+                } else {
+                    Stop();
+                }
+            }
+            _soundPlayer?.Play();
+            IsPlaying = true;
+        }
+
+        public void Stop() {
+            _soundPlayer?.Stop();
+            IsPlaying = false;
+        }
+
+        public void Pause() {
+            if (!IsPlaying || IsPaused) {
+                return;
+            }
+            _soundPlayer?.Pause();
+            IsPaused = true;
+        }
+
+        public TimeSpan CurrentTime {
+            get { return _waveStream.CurrentTime; }
+            set {
+                lock(_syncObject) {
+                    var waveStream = _waveStream;
+                    waveStream.CurrentTime = value;
+                    var position = waveStream.Position;
+                    var blockAlign = waveStream.BlockAlign;
+                    if (position % blockAlign != 0) {
+                        position = (long)(Math.Round(position / (double)blockAlign) * blockAlign);
+                        if (position < 0) {
+                            position = 0;
+                        }
+                        waveStream.Position = position;
+                    }
+                }
+                PositionChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public TimeSpan TotalTime => _waveStream.TotalTime;
+
+        public bool IsPlaying {
+            get {
+                lock (_syncObject) {
+                    return _isPlaying;
+                }
+            }
+            private set {
+                lock (_syncObject) {
+                    _isPlaying = value;
+                }
+            }
+        }
+
+        public bool IsPaused {
+            get {
+                lock (_syncObject) {
+                    return _isPaused;
+                }
+            }
+            private set {
+                lock (_syncObject) {
+                    _isPaused = value;
+                }
+            }
+        }
+        
+        public WaveChannel32 AddInputStream(WaveStream waveStream, float volume = 1f) {
+            lock (_syncObject) {
+                var rateConvertedStream = waveStream;
+                if (NeedSampleRateConversion(waveStream.WaveFormat)) {
+                    rateConvertedStream = new ResamplerDmoStream(waveStream, _waveStream.WaveFormat);
+                }
+                var addedStream = new WaveChannel32(rateConvertedStream, volume, 0f);
+                _waveStream.AddInputStream(addedStream);
+                return addedStream;
+            }
+        }
+
+        public void RemoveInputStream(WaveStream waveStream) {
+            lock (_syncObject) {
+                _waveStream.RemoveInputStream(waveStream);
+            }
+        }
+
+        private bool NeedSampleRateConversion(WaveFormat waveFormat) {
+            if (_waveStream.InputCount == 0) {
+                return false;
+            }
+            return waveFormat.SampleRate != _waveStream.WaveFormat.SampleRate;
+        }
+
+        protected override void Dispose(bool disposing) {
+            _soundPlayer?.Stop();
+            _soundPlayer?.Dispose();
+            _waveStream?.Dispose();
+            _soundPlayer = null;
+            _waveStream = null;
+            _soundPlayer = null;
+        }
+
+        public ScorePlayer() {
+            _syncObject = new object();
+            _waveStream = new WaveMixerStream32();
+            _soundPlayer = new AudioOut(AudioClientShareMode.Shared, 60);
+            _soundPlayer.Init(_waveStream);
+        }
+
+        private WaveMixerStream32 _waveStream;
+        private AudioOut _soundPlayer;
+        private bool _isPlaying;
+        private bool _isPaused;
+        private readonly object _syncObject;
+    }
+}
