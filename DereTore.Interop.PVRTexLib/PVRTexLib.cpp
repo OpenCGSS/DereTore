ef3e399ed8f5185774f45043e384e6595342f6da
George Wu <uiharu@buaa.edu.cn>
2016-11-02 07:11:47 +0100

    Tried to use PVR library to generate DDS textures.

--- DereTore.Interop.PVRTexLib/PVRTexLib.cpp
+++ DereTore.Interop.PVRTexLib/PVRTexLib.cpp
@@ -8,13 +8,18 @@ using namespace System::Diagnostics;
 using namespace System::Drawing;
 using namespace System::Drawing::Imaging;
 using namespace System::Runtime::InteropServices;
+using namespace DereTore::Interop::PVRTexLib;
+
+// TODO: The DDS preset doesn't seem to work...
+MpvrEncodePreset DdsEncPreset{ PVRTGENPIXELID3('b', 'g', 'r', 5, 6, 5), ECompressorQuality::ePVRTCNormal, ".dds", PvrUtilities::DefaultDdsMipLevels };
+MpvrEncodePreset PvrEncPreset{ PixelType(ePVRTPF_ETC2_RGB), ECompressorQuality::eETCFast, ".pvr", PvrUtilities::DefaultPvrMipLevels };
 
 #define PIXEL_FORMAT_GDIPLUS_LIB_USED (PVRTGENPIXELID4('b', 'g', 'r', 'a', 8, 8, 8, 8))
 
-VOID STDCALL MpvrCompressTextureFrom32bppArgb(PVOID pBitmapData, DWORD dwWidth, DWORD dwHeight, DWORD dwMipLevels, OUT PVOID *pCompressedTexture, OUT PDWORD pdwTextureDataSize) {
+VOID STDCALL MpvrCompressPvrTextureFrom32bppArgb(PVOID pBitmapData, DWORD dwWidth, DWORD dwHeight, DWORD dwStride, DWORD dwMipLevels, OUT PVOID *pCompressedTexture, OUT PDWORD pdwTextureDataSize) {
 	PDWORD textureSizes;
 	auto pixelFormat = PIXEL_FORMAT_GDIPLUS_LIB_USED;
-	auto textureData = MpvrCompressTextureEx(pBitmapData, dwWidth, dwHeight, dwMipLevels, pixelFormat, FALSE, OUT &textureSizes);
+	auto textureData = MpvrCompressPvrTexture(pBitmapData, dwWidth, dwHeight, dwStride, dwMipLevels, pixelFormat, FALSE, OUT &textureSizes);
 	if (!pCompressedTexture) {
 		delete textureData;
 	} else {
@@ -26,11 +31,12 @@ VOID STDCALL MpvrCompressTextureFrom32bppArgb(PVOID pBitmapData, DWORD dwWidth, 
 	delete textureSizes;
 }
 
-void MpvrCompressTextureInternal(PVOID pData, UINT32 dwWidth, UINT32 dwHeight, UINT32 dwMipLevels, UINT64 ullPixelFormat, BOOL bIsPremultiplied, OUT pvrtexture::CPVRTexture **ppTexture) {
+void MpvrCompressTextureInternal(PVOID pData, UINT32 dwWidth, UINT32 dwHeight, DWORD dwStride, PixelType eInputPixelType, BOOL bIsPremultiplied,
+	UINT32 dwOutputMipLevels, PixelType eOutputPixelType, ECompressorQuality eQuality, OUT CPVRTexture &ppTexture) {
 	PVRTextureHeaderV3 pvrHeader;
 	pvrHeader.u32Version = PVRTEX_CURR_IDENT;
 	pvrHeader.u32Flags = bIsPremultiplied ? PVRTEX3_PREMULTIPLIED : 0;
-	pvrHeader.u64PixelFormat = ullPixelFormat;
+	pvrHeader.u64PixelFormat = eInputPixelType.PixelTypeID;
 	pvrHeader.u32ColourSpace = EPVRTColourSpace::ePVRTCSpacelRGB;
 	pvrHeader.u32ChannelType = EPVRTVariableType::ePVRTVarTypeUnsignedByteNorm;
 	pvrHeader.u32Width = dwWidth;
@@ -38,77 +44,61 @@ void MpvrCompressTextureInternal(PVOID pData, UINT32 dwWidth, UINT32 dwHeight, U
 	pvrHeader.u32Depth = 1;
 	pvrHeader.u32NumSurfaces = 1;
 	pvrHeader.u32NumFaces = 1;
-	pvrHeader.u32MIPMapCount = dwMipLevels + 1;
+	pvrHeader.u32MIPMapCount = dwOutputMipLevels + 1;
 	pvrHeader.u32MetaDataSize = 0;
 
-	*ppTexture = new pvrtexture::CPVRTexture(pvrHeader, pData);
-	if (dwMipLevels > 0) {
-		pvrtexture::GenerateMIPMaps(**ppTexture, pvrtexture::EResizeMode::eResizeLinear, dwMipLevels + 1);
-	}
-	Debug::Print("Exp. mip levels: " + static_cast<Int32>(dwMipLevels).ToString());
+	PBYTE p = nullptr;
+	// TODO: We choose the format ARGB8888, so stride always equals to width. Ignore this paramater.
+	p = static_cast<PBYTE>(pData);
 
-	// Unity uses ETC2_RGB in its textures ~128x128
-	pvrtexture::PixelType pixelType(ePVRTPF_ETC2_RGB);
-	pvrtexture::Transcode(**ppTexture, pixelType, EPVRTVariableType::ePVRTVarTypeUnsignedByteNorm, EPVRTColourSpace::ePVRTCSpacelRGB, pvrtexture::ECompressorQuality::eETCFast);
+	ppTexture = CPVRTexture(pvrHeader);
+	memcpy(ppTexture.getDataPtr(), p, dwStride * dwHeight);
+	if (p != pData) {
+		delete[] p;
+	}
+	if (dwOutputMipLevels > 0) {
+		GenerateMIPMaps(ppTexture, EResizeMode::eResizeLinear, dwOutputMipLevels + 1);
+	}
+
+	Transcode(ppTexture, eOutputPixelType, EPVRTVariableType::ePVRTVarTypeUnsignedByteNorm, EPVRTColourSpace::ePVRTCSpacelRGB, eQuality);
 }
 
 // Basic idea: https://github.com/SickheadGames/ManagedPVRTC/blob/master/PVRTexLibWrapper/PVRTexLibWrapper.cpp
 /// <summary>
 /// Core function to compress PowerVR textures.
-/// <param name="dwMipLevels">Expected mipmap level. If no mipmapping is needed, pass it 0.</param>
 /// </summary>
-PBYTE STDCALL MpvrCompressTextureEx(PVOID pData, UINT32 dwWidth, UINT32 dwHeight, UINT32 dwMipLevels, UINT64 ullPixelFormat, BOOL bIsPremultiplied, OUT PDWORD *ppdwDataSizes) {
-	pvrtexture::CPVRTexture *texture;
-	MpvrCompressTextureInternal(pData, dwWidth, dwHeight, dwMipLevels, ullPixelFormat, bIsPremultiplied, OUT &texture);
+/// <param name="dwMipLevels">Expected mipmap level. If no mipmapping is needed, pass it 0.</param>
+PBYTE STDCALL MpvrCompressPvrTexture(PVOID pData, UINT32 dwWidth, UINT32 dwHeight, DWORD dwStride, UINT32 dwMipLevels, PixelType eInputPixelType, BOOL bIsPremultiplied, OUT PDWORD *ppdwDataSizes) {
+	CPVRTexture texture;
+	MpvrCompressTextureInternal(pData, dwWidth, dwHeight, dwStride, eInputPixelType, bIsPremultiplied, dwMipLevels, PvrEncPreset.pixelType, PvrEncPreset.quality, OUT texture);
 
 	*ppdwDataSizes = new DWORD[dwMipLevels + 1];
 	for (auto x = 0; x <= dwMipLevels; x++) {
-		(*ppdwDataSizes)[x] = texture->getDataSize(x);
+		(*ppdwDataSizes)[x] = texture.getDataSize(x);
 	}
 
-	auto totalDataSize = texture->getDataSize();
+	auto totalDataSize = texture.getDataSize();
 	auto returnData = new BYTE[totalDataSize];
-	memcpy(returnData, texture->getDataPtr(), totalDataSize);
-	delete texture;
+	memcpy(returnData, texture.getDataPtr(), totalDataSize);
 	return returnData;
 }
 
-BOOL MpvrCompressTextureToFile(PVOID pData, UINT32 dwWidth, UINT32 dwHeight, UINT32 dwMipLevels, UINT64 ullPixelFormat, BOOL bIsPremultiplied, LPCSTR lpstrFileName) {
-	pvrtexture::CPVRTexture *texture;
-	MpvrCompressTextureInternal(pData, dwWidth, dwHeight, dwMipLevels, ullPixelFormat, bIsPremultiplied, OUT &texture);
+BOOL MpvrCompressTextureToFile(PVOID pData, UINT32 dwWidth, UINT32 dwHeight, DWORD dwStride, PixelType eInputPixelType, BOOL bIsPremultiplied, MpvrEncodePreset &preset, LPCSTR lpstrFileName) {
+	CPVRTexture texture;
+	MpvrCompressTextureInternal(pData, dwWidth, dwHeight, dwStride, eInputPixelType, bIsPremultiplied, preset.mipLevels, preset.pixelType, preset.quality, OUT texture);
 	CPVRTString fileName(lpstrFileName);
-	auto succeeded = texture->saveFile(fileName);
-	delete texture;
+	auto succeeded = texture.saveFile(fileName);
+	Debug::Print("Saving to " + gcnew String(lpstrFileName) + " successful: " + succeeded);
 	return succeeded;
 }
 
 DECL_NS_BEGIN_MPVR()
 
-array<BYTE> ^PvrUtilities::CompressTextureFrom32bppArgb(IntPtr bitmapData, UINT32 width, UINT32 height, DWORD mipLevels) {
-	DWORD dataSize;
-	PVOID texture;
-	MpvrCompressTextureFrom32bppArgb(static_cast<PVOID>(bitmapData), width, height, mipLevels, OUT &texture, OUT &dataSize);
-	auto compressedTexture = gcnew array<BYTE>(dataSize);
-	Marshal::Copy(IntPtr(texture), compressedTexture, 0, dataSize);
-	delete texture;
-	return compressedTexture;
-}
-
-DWORD PvrUtilities::GetTextureDataSizeFrom32bppArgb(IntPtr bitmapData, UINT32 width, UINT32 height, DWORD mipLevels) {
-	DWORD dataSize;
-	MpvrCompressTextureFrom32bppArgb(static_cast<PVOID>(bitmapData), width, height, mipLevels, nullptr, OUT &dataSize);
-	return dataSize;
-}
-
-DWORD PvrUtilities::GetTextureDataSizeFrom32bppArgb(IntPtr bitmapData, UINT32 width, UINT32 height) {
-	return GetTextureDataSizeFrom32bppArgb(bitmapData, width, height, DefaultMipLevels);
-}
-
-array<BYTE> ^PvrUtilities::GetFullCompressedTextureFrom32bppArgb(IntPtr bitmapData, UINT32 width, UINT32 height, DWORD mipLevels) {
+array<BYTE> ^PvrUtilities::GetTextureFrom32bppArgb(IntPtr bitmapData, UINT32 width, UINT32 height, DWORD stride, MpvrEncodePreset &preset) {
 	array<BYTE> ^textureFileData = nullptr;
 	CHAR tempPath[MAX_PATH] = { 0 }, tempFileName[MAX_PATH] = { 0 };
 	GetTempPath(MAX_PATH, tempPath);
-	GetTempFileName(tempPath, "pvr", 0, tempFileName);
+	GetTempFileName(tempPath, "tex", 0, tempFileName);
 	for (auto i = strlen(tempFileName) - 1; i > 0; --i) {
 		if (tempFileName[i] == '.') {
 			tempFileName[i] = '\0';
@@ -117,10 +107,10 @@ array<BYTE> ^PvrUtilities::GetFullCompressedTextureFrom32bppArgb(IntPtr bitmapDa
 			break;
 		}
 	}
-	strcat(tempFileName, ".pvr");
+	strcat(tempFileName, preset.extension);
 	PDWORD textureSizes;
 	auto originalPvrPixelFormat = PIXEL_FORMAT_GDIPLUS_LIB_USED;
-	auto textureData = MpvrCompressTextureToFile(static_cast<PVOID>(bitmapData), width, height, mipLevels, originalPvrPixelFormat, FALSE, tempFileName);
+	auto textureData = MpvrCompressTextureToFile(static_cast<PVOID>(bitmapData), width, height, stride, originalPvrPixelFormat, FALSE, preset, tempFileName);
 	// TODO: Any other solutions?
 	auto hFile = CreateFile(tempFileName, GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, nullptr);
 	if (hFile && hFile != INVALID_HANDLE_VALUE) {
@@ -140,19 +130,22 @@ array<BYTE> ^PvrUtilities::GetFullCompressedTextureFrom32bppArgb(IntPtr bitmapDa
 	return textureFileData;
 }
 
-array<BYTE> ^PvrUtilities::GetFullTextureFromImage(Bitmap ^bitmap) {
-	return GetFullTextureFromImage(bitmap, DefaultMipLevels);
-}
-
-array<BYTE> ^PvrUtilities::GetFullTextureFromImage(Bitmap ^bitmap, DWORD mipLevels) {
+array<BYTE> ^PvrUtilities::GetTextureFromImage(Bitmap ^bitmap, MpvrEncodePreset &preset) {
 	auto actualBitmap = dynamic_cast<Bitmap ^>(bitmap->Clone());
 	// Don't have any idea. Unity just does it this way.
 	actualBitmap->RotateFlip(RotateFlipType::RotateNoneFlipY);
 	auto bitmapData = actualBitmap->LockBits(System::Drawing::Rectangle(0, 0, actualBitmap->Width, actualBitmap->Height), ImageLockMode::ReadOnly, PixelFormat::Format32bppArgb);
-	auto textureData = GetFullCompressedTextureFrom32bppArgb(bitmapData->Scan0, bitmapData->Width, bitmapData->Height, mipLevels);
+	auto textureData = GetTextureFrom32bppArgb(bitmapData->Scan0, bitmapData->Width, bitmapData->Height, bitmapData->Stride, preset);
 	actualBitmap->UnlockBits(bitmapData);
 	delete actualBitmap;
 	return textureData;
+}
+
+array<BYTE> ^PvrUtilities::GetPvrTextureFromImage(Bitmap ^bitmap) {
+	return GetTextureFromImage(bitmap, PvrEncPreset);
+}
+array<BYTE> ^PvrUtilities::GetDdsTextureFromImage(Bitmap ^bitmap) {
+	return GetTextureFromImage(bitmap, DdsEncPreset);
 }
 
 DECL_NS_END_MPVR()
--- DereTore.Interop.PVRTexLib/PVRTexLib.h
+++ DereTore.Interop.PVRTexLib/PVRTexLib.h
@@ -2,25 +2,33 @@
 #include "mpvr_header.h"
 #include <Windows.h>
 
+using namespace pvrtexture;
+
 DECL_NS_BEGIN_MPVR()
+
+struct MpvrEncodePreset {
+	PixelType pixelType;
+	ECompressorQuality quality;
+	LPCSTR extension;
+	DWORD mipLevels;
+};
 
 public ref class PvrUtilities abstract sealed {
 
 public:
-	static DWORD GetTextureDataSizeFrom32bppArgb(System::IntPtr bitmapData, UINT32 width, UINT32 height);
-	static DWORD GetTextureDataSizeFrom32bppArgb(System::IntPtr bitmapData, UINT32 width, UINT32 height, DWORD mipLevels);
-	static array<BYTE> ^GetFullTextureFromImage(System::Drawing::Bitmap ^bitmap);
-	static array<BYTE> ^GetFullTextureFromImage(System::Drawing::Bitmap ^bitmap, DWORD mipLevels);
+	static array<BYTE> ^GetPvrTextureFromImage(System::Drawing::Bitmap ^bitmap);
+	static array<BYTE> ^GetDdsTextureFromImage(System::Drawing::Bitmap ^bitmap);
+
+	static initonly DWORD DefaultPvrMipLevels = 6;
+	static initonly DWORD DefaultDdsMipLevels = 7;
 
 private:
-	static array<BYTE> ^CompressTextureFrom32bppArgb(System::IntPtr bitmapData, UINT32 width, UINT32 height, DWORD mipLevels);
-	static array<BYTE> ^GetFullCompressedTextureFrom32bppArgb(System::IntPtr bitmapData, UINT32 width, UINT32 height, DWORD mipLevels);
-
-	static DWORD DefaultMipLevels = 6;
+	static array<BYTE> ^GetTextureFrom32bppArgb(System::IntPtr bitmapData, UINT32 width, UINT32 height, DWORD stride, MpvrEncodePreset &preset);
+	static array<BYTE> ^GetTextureFromImage(System::Drawing::Bitmap ^bitmap, MpvrEncodePreset &preset);
 
 };
 
 DECL_NS_END_MPVR()
 
-MPVR_EXPORTS(VOID)  MpvrCompressTextureFrom32bppArgb(PVOID pBitmapData, DWORD dwWidth, DWORD dwHeight, DWORD dwMipLevels, OUT PVOID *pCompressedTexture, OUT PDWORD pdwTextureDataSize);
-MPVR_EXPORTS(PBYTE) MpvrCompressTextureEx(PVOID pData, UINT32 dwWidth, UINT32 dwHeight, UINT32 dwMipLevels, UINT64 ullPixelFormat, BOOL bIsPremultiplied, OUT PDWORD *ppdwDataSizes);
+MPVR_EXPORTS(VOID)  MpvrCompressPvrTextureFrom32bppArgb(PVOID pBitmapData, DWORD dwWidth, DWORD dwHeight, DWORD dwStride, DWORD dwMipLevels, OUT PVOID *pCompressedTexture, OUT PDWORD pdwTextureDataSize);
+MPVR_EXPORTS(PBYTE) MpvrCompressPvrTexture(PVOID pData, UINT32 dwWidth, UINT32 dwHeight, DWORD dwStride, UINT32 dwMipLevels, PixelType eInputPixelType, BOOL bIsPremultiplied, OUT PDWORD *ppdwDataSizes);
