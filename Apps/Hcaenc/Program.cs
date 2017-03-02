7a35d4ae5e14dcfa70bb9cb81cc469f026f08e10
George Wu <uiharu@buaa.edu.cn>
2017-03-02 22:11:21 +0100

    hcaenc 64-bit build.

--- Apps/Hcaenc/Program.cs
+++ Apps/Hcaenc/Program.cs
@@ -5,6 +5,10 @@ namespace DereTore.Apps.Hcaenc {
     internal static class Program {
 
         private static int Main(string[] args) {
+            if (Environment.Is64BitProcess) {
+                Console.WriteLine(UnsupportedBuildMessage);
+                return -2;
+            }
             if (args.Length != 2) {
                 Console.WriteLine(HelpMessage);
                 return -1;
@@ -18,6 +22,7 @@ namespace DereTore.Apps.Hcaenc {
         private static extern int hcaencEncodeToFile([MarshalAs(UnmanagedType.LPStr)] string lpstrInputFile, [MarshalAs(UnmanagedType.LPStr)] string lpstrOutputFile, int nQuality, int nCutoff, ulong ullKey);
 
         private static readonly string HelpMessage = "Usage: hcaenc.exe <input WAVE> <output HCA>";
+        private static readonly string UnsupportedBuildMessage = "hcaenc only has 32-bit version due to the limits of hcaenc_lite.dll.";
 
     }
 }
