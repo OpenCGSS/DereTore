1e801f89e9da7751d74e0fb615298cdf4f63399a
George Wu <uiharu@buaa.edu.cn>
2018-02-09 18:39:13 +0100

    Updated packages.

--- Apps/Hca2Wav/Options.cs
+++ Apps/Hca2Wav/Options.cs
@@ -5,31 +5,31 @@ using DereTore.Exchange.Audio.HCA;
 namespace DereTore.Apps.Hca2Wav {
     public sealed class Options {
 
-        [ValueOption(0)]
+        [Value(0)]
         public string InputFileName { get; set; } = string.Empty;
 
         [Option('o', "out", HelpText = "Output file name", Required = false)]
         public string OutputFileName { get; set; } = string.Empty;
 
-        [Option('a', "key1", HelpText = "Key 1 (8 hex digits)", Required = false, DefaultValue = "f27e3b22")]
+        [Option('a', "key1", HelpText = "Key 1 (8 hex digits)", Required = false, Default = "f27e3b22")]
         public string Key1 { get; set; } = CgssCipher.Key1.ToString("x8");
 
-        [Option('b', "key2", HelpText = "Key 2 (8 hex digits)", Required = false, DefaultValue = "00003657")]
+        [Option('b', "key2", HelpText = "Key 2 (8 hex digits)", Required = false, Default = "00003657")]
         public string Key2 { get; set; } = CgssCipher.Key2.ToString("x8");
 
-        [Option("infinite", HelpText = "Enables infinite loop", Required = false, DefaultValue = false)]
+        [Option("infinite", HelpText = "Enables infinite loop", Required = false, Default = false)]
         public bool InfiniteLoop { get; set; } = AudioParams.Default.InfiniteLoop;
 
-        [Option('l', "loop", HelpText = "Number of simulated loops", Required = false, DefaultValue = 0u)]
+        [Option('l', "loop", HelpText = "Number of simulated loops", Required = false, Default = 0u)]
         public uint SimulatedLoopCount { get; set; } = AudioParams.Default.SimulatedLoopCount;
 
-        [Option('e', "no-header", HelpText = "Do not emit wave header", Required = false, DefaultValue = false)]
+        [Option('e', "no-header", HelpText = "Do not emit wave header", Required = false, Default = false)]
         public bool NoWaveHeader { get; set; }
 
-        [Option("overrides-cipher", HelpText = "Overrides original cipher type", Required = false, DefaultValue = false)]
+        [Option("overrides-cipher", HelpText = "Overrides original cipher type", Required = false, Default = false)]
         public bool OverridesCipherType { get; set; }
 
-        [Option('c', "cipher", HelpText = "Overridden cipher type", Required = false, DefaultValue = 0u)]
+        [Option('c', "cipher", HelpText = "Overridden cipher type", Required = false, Default = 0u)]
         public uint OverriddenCipherType { get; set; }
 
     }
--- Apps/JacketCreator/Options.cs
+++ Apps/JacketCreator/Options.cs
@@ -1,4 +1,4 @@
-﻿using CommandLine;
+using CommandLine;
 
 namespace DereTore.Apps.JacketCreator {
     internal sealed class Options {
@@ -6,16 +6,16 @@ namespace DereTore.Apps.JacketCreator {
         [Option("image", HelpText = "Expected image.", Required = true)]
         public string ImageFileName { get; set; }
 
-        [Option("song", DefaultValue = 1001, HelpText = "The song ID. Example: 1001", Required = false)]
+        [Option("song", Default = 1001, HelpText = "The song ID. Example: 1001", Required = false)]
         public int SongID { get; set; }
 
         [Option('o', "output", HelpText = "The output directory of generated jacket file.", Required = true)]
         public string OutputDirectory { get; set; }
 
-        [Option("path_id_m", DefaultValue = 0x547e3042158b3095, HelpText = "Path ID for medium-sized texture.", Required = false)]
+        [Option("path_id_m", Default = 0x547e3042158b3095, HelpText = "Path ID for medium-sized texture.", Required = false)]
         public long DdsPathID { get; set; }
 
-        [Option("path_id_s", DefaultValue = unchecked((long)0xed362ad73c325b56), HelpText = "Path ID for small-sized texture.", Required = false)]
+        [Option("path_id_s", Default = unchecked((long)0xed362ad73c325b56), HelpText = "Path ID for small-sized texture.", Required = false)]
         public long PvrPathID { get; set; }
 
     }
