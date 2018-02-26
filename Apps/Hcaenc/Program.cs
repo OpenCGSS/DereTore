5974a6cf6d15f58ddb5ccdf455493c9434958217
George Wu <uiharu@buaa.edu.cn>
2018-02-26 02:09:23 +0100

    [hcaenc] Added quality support.

--- Apps/Hcaenc/Program.cs
+++ Apps/Hcaenc/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using CommandLine;
 
 namespace DereTore.Apps.Hcaenc {
     internal static class Program {
@@ -11,29 +12,40 @@ namespace DereTore.Apps.Hcaenc {
                 return -2;
             }
 
-            if (args.Length < 1) {
+            var parser = new Parser(settings => { settings.IgnoreUnknownArguments = true; });
+
+            var parserResult = parser.ParseArguments<Options>(args);
+
+            if (parserResult.Tag == ParserResultType.NotParsed) {
                 Console.WriteLine(HelpMessage);
                 return -1;
             }
 
-            var inputFile = args[0];
+            var options = ((Parsed<Options>)parserResult).Value;
 
-            inputFile = Path.GetFullPath(inputFile);
+            var inputFile = Path.GetFullPath(options.InputFileName);
 
             string outputFile;
 
-            if (args.Length >= 2) {
-                outputFile = args[1];
+            if (!string.IsNullOrEmpty(options.OutputFileName)) {
+                outputFile = options.OutputFileName;
             } else {
                 var inputFileInfo = new FileInfo(inputFile);
                 var inputFileDir = inputFileInfo.DirectoryName;
                 outputFile = Path.Combine(inputFileDir, inputFileInfo.Name.Substring(0, inputFileInfo.Name.Length - inputFileInfo.Extension.Length) + ".hca");
             }
 
-            Console.WriteLine("Encoding {0} to {1} ...", inputFile, outputFile);
+            var quality = options.Quaility;
+
+            if (quality < 1 || quality > 5) {
+                Console.WriteLine("Warning: Quality should be 1 to 5. Using q=1.");
+                quality = 1;
+            }
+
+            Console.WriteLine("Encoding {0} to {1} (q={2}) ...", inputFile, outputFile, quality);
 
             // Quality = 3 (~128 Kbps) for MLTD, 1 (~256 Kbps) for CGSS
-            int quality = 3, cutoff = 0;
+            int cutoff = 0;
             ulong key = 0;
 
             return hcaencEncodeToFile(inputFile, outputFile, quality, cutoff, key);
@@ -42,7 +54,7 @@ namespace DereTore.Apps.Hcaenc {
         [DllImport("hcaenc_lite", CallingConvention = CallingConvention.StdCall)]
         private static extern int hcaencEncodeToFile([MarshalAs(UnmanagedType.LPStr)] string lpstrInputFile, [MarshalAs(UnmanagedType.LPStr)] string lpstrOutputFile, int nQuality, int nCutoff, ulong ullKey);
 
-        private static readonly string HelpMessage = "Usage: hcaenc.exe <input WAVE> [<output HCA>]";
+        private static readonly string HelpMessage = "Usage: hcaenc.exe <input WAVE> [<output HCA> -q <quality>]";
         private static readonly string UnsupportedBuildMessage = "hcaenc only has 32-bit version due to the limits of hcaenc_lite.dll.";
 
     }
